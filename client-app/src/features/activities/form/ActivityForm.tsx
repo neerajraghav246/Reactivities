886dd90e518c01ca1c91c8e21c556cf455e1faea
neerajraghav246 <neeraj.raghav246@gmail.com>
2022-06-08 15:11:15 +0530

    End of section 15

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -11,24 +11,16 @@ import MyTextArea from "../../../app/common/form/MyTextArea";
 import MySelectInput from "../../../app/common/form/MySelectInput";
 import { categoryOptions } from "../../../app/common/options/categoryOptions";
 import MyDateInput from "../../../app/common/form/MyDateInput";
-import { Activity } from "../../../app/models/activity";
+import { ActivityFormValues } from "../../../app/models/activity";
 import { history } from "../../..";
 import { v4 as uuid } from 'uuid';
 export default observer(function ActivityForm() {
 
     const { activityStore } = useStore();
-    const { loading, loadActivity, loadingInitial, createActivity, updateActivity} = activityStore;
+    const { loadActivity, loadingInitial, createActivity, updateActivity} = activityStore;
     const { id } = useParams<{ id: string }>();
 
-    const [activity, setActivity] = useState<Activity>({
-        id: '',
-        title: '',
-        category: '',
-        description: '',
-        date: null,
-        city: '',
-        venue: ''
-    });
+    const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
     const validationSchema = Yup.object(
         {
@@ -43,12 +35,12 @@ export default observer(function ActivityForm() {
 
     useEffect(() => {
         if (id) loadActivity(id).then((activity) => {
-            setActivity(activity!);
+            setActivity(new ActivityFormValues(activity));
         })
     }, [id, loadActivity])
 
-    function handleFormSubmit(activity: Activity) {
-        if (activity.id.length === 0) {
+    function handleFormSubmit(activity: ActivityFormValues) {
+        if (!activity.id) {
             let newActivity = { ...activity, id: uuid() };
             createActivity(newActivity).then(() => {
                 history.push(`/activities/${newActivity.id}`);
@@ -82,7 +74,7 @@ export default observer(function ActivityForm() {
                         <MyTextInput placeholder='Venue' name='venue' />
                         <Button
                             disabled={isSubmitting || !dirty || !isValid}
-                            loading={loading} floated="right" positive type='submit' content='Submit' />
+                            loading={isSubmitting} floated="right" positive type='submit' content='Submit' />
                         <Button as={Link} to='/activities' floated="right" type='cancel' content='Cancel' />
                     </Form>
                 )}
